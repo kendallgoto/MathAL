3564fad04f0f939ba5bbf5977aa8fe45c8e29dd7
Kendall Goto <31294853+kendallgoto@users.noreply.github.com>
2018-01-09 00:40:34 -0800

    v1.0

---
+++ Research Notes.md
@@ -0,0 +1,6 @@
+$('.responseContainer .text') -> all on-screen response blobs
+parse for MathAL inserts
+divider: ~
+~~\latex~(2x+3)~
+
+$('.questionTagAnswer[style="display: block;"] > div[contenteditable=true]') is text box (with a terrible way to find it, included!)
---
+++ src/js/ALMath.js
@@ -0,0 +1,112 @@
+var observeDOM = (function(){
+    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver,
+        eventListenerSupported = window.addEventListener;
+    return function(obj, callback){
+        if( MutationObserver ){
+            var obs = new MutationObserver(function(mutations, observer){
+                if( mutations[0].addedNodes.length || mutations[0].removedNodes.length ) 
+                    callback();
+            });
+            obs.observe( obj, { attributes:false, childList:true, subtree:false });
+        }
+    };
+})();
+var MQ = MathQuill.getInterface(2);
+
+$(function() {
+	//var loadObserveOnce = 0;
+	captureTextWindows();
+	var refresh = setInterval(function() {
+		console.log("checking elements");
+			// observeDOM(document.getElementsByClassName('textSectionContainer')[0], function() {
+			// 	console.log("mutate detected");
+			// 	captureTextWindows();
+			// });
+		captureTextWindows();
+		convertMathIfPresent();
+	}, 1500);
+});
+function captureTextWindows() {
+	$('.questionTagAnswer[style="display: block;"] > div[contenteditable=true]').off('focus').focus(function() {
+		console.log("Focused!");
+		insertFloatingMath(this);
+	}).blur(function() {
+		console.log("Unfocused!");
+		removeFloatingMath(this);
+	});
+}
+function insertFloatingMath(atEle) {
+	//new element for clickin
+	//inside .questionTagAnswer -->
+	var trig = $('<div class="mathALTrigger">M+</div>');
+	$(atEle).data('trigger', trig);
+	trig.appendTo($(atEle).closest(".questionTagAnswer"));
+	mathClickRegister();
+	
+}
+function removeFloatingMath(atEle) {
+	if($(atEle).data('trigger')) {
+		$(atEle).data('trigger').remove();
+	}
+}
+function mathClickRegister() {
+	$('.mathALTrigger').off('mousedown').on('mousedown', function() {
+		//open the popup!
+		console.log("aa");
+		$('.mathALPopup').remove();
+		var mathWindow = $('<div class="mathALPopup"><div class="mathWindow"><span id="math-field"></span><div class="goBtn">&rarr;</div></div></div>');
+		mathWindow.appendTo('body');
+		var field = MQ.MathField(document.getElementById('math-field'), {
+			spaceBehavesLikeTab: true,
+			autoCommands: 'pi sqrt',
+			autoOperatorNames: 'sin cos tan',
+			handlers: {
+				/*
+				enter: function() {
+					enterCommand();
+				},
+				edit: function(msg) {
+					if(field.latex() == "\\sqrt{2}" && latexPrev == "\\sqrt{ }")
+						field.moveToRightEnd();
+					if(field.latex() == "\\sqrt{3}" && latexPrev == "\\sqrt{ }")
+						field.moveToRightEnd();
+					latexPrev = field.latex();
+				}
+				*/
+			}
+		});
+		$('.mathALPopup').click(function(e) {
+			if(e.currentTarget == e.target)
+				dismissALPop();
+		});
+		$('.goBtn').click(function() {
+			//process!
+			var toText = field.text();
+			var toLatex = field.latex();
+			$('.mathWindow').children().remove();
+			$('<textarea class="mathResult"></textarea><div class="mathDesc">Ctrl/Cmd+C!</div>').appendTo('.mathWindow');
+			$('.mathResult').val("~~"+toLatex+"~\r\n"+toText+"\r\n(MathAL Chrome Extension)~");
+			$('.mathWindow').css('flex-direction', 'column');
+			$('.mathResult').select();
+		})
+	});
+}
+function dismissALPop() {
+	$('.mathALPopup').remove();
+}
+
+function convertMathIfPresent() {
+	//Make latex into a viewable form!
+	$('.responseContainer .text').each(function(indx, ele){
+		if($(ele).text().indexOf('~~') == -1)
+			return true;
+		//find our text blob
+		var mainLatex = $(':contains(~~)', ele);
+		var plaintext = mainLatex.next();
+		var ad = plaintext.next();
+		plaintext.remove();
+		ad.remove();
+		mainLatex.text(mainLatex.text().replace("~~", "").replace("~",""));
+		MQ.StaticMath(mainLatex.get(0));
+	})
+}
